98e5a7f185655907029a1cf35c5911dc0ce4c656
Michelle Santiago <msantiago2222@yahoo.com>
2019-10-01 09:38:44 -0700

    added productinfor for product table

---
+++ bamazonSeeds.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS bamazon_db;
+CREATE DATABASE bamazon_db;
+USE bamazon_db;
+
+CREATE TABLE products(
+    item_id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(100) NOT NULL,
+    department_name VARCHAR(100) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    stock_quantity INT NOT NULL,
+    PRIMARY KEY(id)
+);
+
